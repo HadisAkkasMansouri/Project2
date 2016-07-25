b5425f35591d91d996ea46dda812afb6dbd9ca3e
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-07-25 18:35:28 +0430

    hi

--- src/main/java/ir/dotin/server/business/ServerHandler.java
+++ src/main/java/ir/dotin/server/business/ServerHandler.java
@@ -14,48 +14,7 @@ public class ServerHandler{
 
     ResponseTransaction responseTransaction = null;
     Logger logger = Logger.getLogger("Log");
-    Server server = new Server();
-    List<Deposit> depositList = new ArrayList<>();
-    private final static String core_path = "core.json";
-    JSONParser jsonParser = new JSONParser();
 
-    public List<Deposit> readJSONFile() {
-
-        try {
-            Object object = jsonParser.parse(new FileReader(core_path));
-            JSONObject jsonObject = (JSONObject) object;
-            JSONArray jsonArray = (JSONArray) jsonObject.get("deposits");
-            Iterator<Object> iterator = jsonArray.iterator();
-            while (iterator.hasNext()) {
-                Deposit deposit = new Deposit();
-                JSONObject pars = (JSONObject) iterator.next();
-                deposit.setId(Integer.valueOf(pars.get("id").toString()));
-                deposit.setName((String) pars.get("customer"));
-                deposit.setInitalBalance((Integer.valueOf((String) pars.get("initialBalance"))));
-                deposit.setUpperBound((Integer.valueOf((String) pars.get("upperBound"))));
-                depositList.add(deposit);
-            }
-            return depositList;
-        } catch (Exception e) {
-            logger.severe(e.getMessage());
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public String readPortFromFile(){
-        Object object = null;
-        String port = null;
-        try {
-            object = jsonParser.parse(new FileReader(core_path));
-            JSONObject jsonObject = (JSONObject) object;
-            server.setPort(String.valueOf(jsonObject.get("port")));
-            port = server.getPort();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return port;
-    }
 
     public ResponseTransaction receiveFromClient(ObjectInputStream ois) throws IOException, ClassNotFoundException {
         TransactionAccomplishment transactionAccomplishment = new TransactionAccomplishment();
