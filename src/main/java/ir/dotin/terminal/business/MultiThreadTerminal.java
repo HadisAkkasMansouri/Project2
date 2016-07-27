659a3aba50ed41697c47b1ba4aba15118cb8d9fc
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-07-27 10:36:47 +0430

    hi

--- src/main/java/ir/dotin/terminal/business/MultiThreadTerminal.java
+++ src/main/java/ir/dotin/terminal/business/MultiThreadTerminal.java
@@ -2,6 +2,12 @@ package ir.dotin.terminal.business;
 
 public class MultiThreadTerminal {
 
+    private final int threadNumbers;
+
+    public MultiThreadTerminal(int threadNumbers) {
+        this.threadNumbers = threadNumbers;
+    }
+
     public static void main(String[] args) {
         TerminalHandler terminalHandler = new TerminalHandler();
         TerminalInfo.fromXML();
